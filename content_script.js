97a787a26df75653a299591fd583a8f1d8f368b5
Armaan Sharma <sharmaarmaan.9871@gmail.com>
2025-06-24 18:10:54 +0530

    just a small change in ux (clicking outside) now was applying triggering of extension when a text box is in focus but could not quiet make the logic

--- content_script.js
+++ content_script.js
@@ -1,6 +1,12 @@
 // Put all the javascript code here, that you want to execute after page load.
 console.log("contentscript is running");
+let currentMode=null;
+chrome.storage.sync.get("mode",({mode})=>{
+    currentMode=mode;
+})
 chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
+    currentMode=request.setMode;
+   
     if(request.setMode==="habit"){
 //normal habit mode
 console.log("mode is habit");
@@ -19,6 +25,7 @@ console.log("mode is habit");
             }
         });
     }
+    
     else if(request.setMode==="advanced"){
         console.log("mode is advanced");
         const existingblock=document.getElementById("smart-input-box");
@@ -26,5 +33,49 @@ console.log("mode is habit");
             existingblock.remove();
             console.log("block removed as mode changed");
         }
+    }
+    
+});
+ 
+document.addEventListener("click", function (event){
+    const clickede=event.target;
+    
+    if(clickede.tagName==="INPUT"||clickede.tagName==="TEXTAREA"){
+        const input_box=document.getElementById("smart-input-box");
+        if(input_box &&!input_box.contains(event.target)){
+            input_box.remove();
+            console.log("text box removed on outside click");
+        }
+        if(currentMode==="habit"){
+          
+            console.log("mode is habit");
+    const floating_input=document.createElement("textarea");
+    floating_input.placeholder="Type here...";
+    floating_input.id="smart-input-box";
+    document.body.appendChild(floating_input);
+    
+    floating_input.addEventListener("input",()=>{
+        const page_input=document.querySelector("input[type='text'],textarea");
+        if(page_input){
+            page_input.value=floating_input.value;
+        }
+        else{
+            console.warn("No input field found on the page");
+        }
+    });
+        }
+        else if (mode==="advanced"){
+            console.log("mode is advanced");
+            const existingblock=document.getElementById("smart-input-box");
+            if(existingblock){
+                existingblock.remove();
+                console.log("block removed as mode changed");
+            }
+        }
+    }
+    const input_box=document.getElementById("smart-input-box");
+    if(input_box &&!input_box.contains(event.target)){
+        input_box.remove();
+        console.log("text box removed on outside click");
     }
 });
