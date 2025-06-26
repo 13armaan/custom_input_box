40d274e65d22db77b8090186aefe53a81545bb72
Armaan Sharma <sharmaarmaan.9871@gmail.com>
2025-06-27 00:56:09 +0530

    triggered the extension by clicking on the text box on the web page and focus comes to our text box

--- background_script.js
+++ background_script.js
@@ -1,1 +1,11 @@
 // Put all the javascript code here, that you want to execute in background.
+chrome.runtime.onStartup.addListener(()=>{
+    chrome.storage.sync.set({mode:"habit"});
+    console.log("default mode set to habit on startup");
+});
+chrome.runtime.onInstalled.addListener(()=>{
+    chrome.storage.sync.set({mode:"habit"});
+
+    console.log("default mode set to habit on installation");
+    
+});
--- content_script.js
+++ content_script.js
@@ -7,55 +7,42 @@ chrome.storage.sync.get("mode",({mode})=>{
 chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
     currentMode=request.setMode;
    
-    if(request.setMode==="habit"){
+    if(currentMode==="habit"){
 //normal habit mode
-console.log("mode is habit");
-        const floating_input=document.createElement("textarea");
-        floating_input.placeholder="Type here...";
-        floating_input.id="smart-input-box";
-        document.body.appendChild(floating_input);
-        
-        floating_input.addEventListener("input",()=>{
-            const page_input=document.querySelector("input[type='text'],textarea");
-            if(page_input){
-                page_input.value=floating_input.value;
-            }
-            else{
-                console.warn("No input field found on the page");
-            }
-        });
+        console.log("mode changed to habit");
     }
-    
-    else if(request.setMode==="advanced"){
-        console.log("mode is advanced");
-        const existingblock=document.getElementById("smart-input-box");
-        if(existingblock){
-            existingblock.remove();
-            console.log("block removed as mode changed");
-        }
+    else if(currentMode==="advanced"){
+        console.log("mode changed to advanced");
+        const input_box=document.getElementById("smart-input-box");
+    if(input_box) {input_box.remove();
+        console.log("text box removed on advanced mode");}
     }
-    
-});
+    });
+
  
 document.addEventListener("click", function (event){
     const clickede=event.target;
-    
-    if(clickede.tagName==="INPUT"||clickede.tagName==="TEXTAREA"){
-        const input_box=document.getElementById("smart-input-box");
-        if(input_box &&!input_box.contains(event.target)){
+    const istext=clickede.tagName==="INPUT"||clickede.tagName==="TEXTAREA";
+    const input_box=document.getElementById("smart-input-box");
+    if(input_box && !input_box.contains(event.target) && !istext){
+      
             input_box.remove();
             console.log("text box removed on outside click");
-        }
-        if(currentMode==="habit"){
-          
+        
+    }
+    else if(istext && currentMode==="habit"){
+        if(input_box) input_box.remove();
+       
+          page_input=clickede;
             console.log("mode is habit");
     const floating_input=document.createElement("textarea");
     floating_input.placeholder="Type here...";
     floating_input.id="smart-input-box";
     document.body.appendChild(floating_input);
-    
+    floating_input.focus();
+    console.log("text box added");
     floating_input.addEventListener("input",()=>{
-        const page_input=document.querySelector("input[type='text'],textarea");
+         
         if(page_input){
             page_input.value=floating_input.value;
         }
@@ -64,18 +51,7 @@ document.addEventListener("click", function (event){
         }
     });
         }
-        else if (mode==="advanced"){
-            console.log("mode is advanced");
-            const existingblock=document.getElementById("smart-input-box");
-            if(existingblock){
-                existingblock.remove();
-                console.log("block removed as mode changed");
-            }
-        }
-    }
-    const input_box=document.getElementById("smart-input-box");
-    if(input_box &&!input_box.contains(event.target)){
-        input_box.remove();
-        console.log("text box removed on outside click");
-    }
+       
+    
+    
 });
